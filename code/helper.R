379693beb0bcdcd99af60bc86077543e30f940d2
Zsombor Hegedus <zsomborhegedus@gmail.com>
2021-01-31 11:06:07 +0100

    add helper R code

---
+++ code/helper.R
@@ -0,0 +1,60 @@
+# source: https://osf.io/7epdj/
+price_diff_by_variables2 <- function(df, factor_var, dummy_var, factor_lab, dummy_lab){
+    # Looking for interactions.
+    # It is a function it takes 3 arguments: 1) Your dataframe,
+    # 2) the factor variable (like room_type)
+    # 3)the dummy variable you are interested in (like TV)
+    
+    # Process your data frame and make a new dataframe which contains the stats
+    factor_var <- as.name(factor_var)
+    dummy_var <- as.name(dummy_var)
+    
+    stats <- df %>%
+        group_by(!!factor_var, !!dummy_var) %>%
+        dplyr::summarize(Mean = mean(price, na.rm=TRUE),
+                         se = sd(price)/sqrt(n()))
+    
+    stats[,2] <- lapply(stats[,2], factor)
+    
+    ggplot(stats, aes_string(colnames(stats)[1], colnames(stats)[3], fill = colnames(stats)[2]))+
+        geom_bar(stat='identity', position = position_dodge(width=0.9), alpha=0.8)+
+        geom_errorbar(aes(ymin=Mean-(1.96*se),ymax=Mean+(1.96*se)),
+                      position=position_dodge(width = 0.9), width = 0.25)+
+        scale_color_manual(name=dummy_lab,
+                           values=c(color[2],color[1])) +
+        scale_fill_manual(name=dummy_lab,
+                          values=c(color[2],color[1])) +
+        ylab('Mean Price')+
+        xlab(factor_lab) +
+        theme_bg()+
+        theme(panel.grid.major=element_blank(),
+              panel.grid.minor=element_blank(),
+              panel.border=element_blank(),
+              axis.line=element_line(),
+              legend.position = "top",
+              #legend.position = c(0.7, 0.9),
+              legend.box = "vertical",
+              legend.text = element_text(size = 5),
+              legend.title = element_text(size = 5, face = "bold"),
+              legend.key.size = unit(x = 0.4, units = "cm")
+        )
+}
+
+
+get_lns <- function(df, cont_vars, skewness_th = .5) {
+    
+    skews <- sapply(cont_vars,function(x) {
+        skewness(df[,x])
+    })
+    
+    names(skews) <- cont_vars
+    
+    for (i in seq_along(skews)){
+        if(abs(skews[[i]]) > skewness_th){
+            df[,paste0('ln_',names(skews)[i])] <- log(df[,names(skews)[i]])
+        }
+    }
+    
+    return(df)
+    
+}
